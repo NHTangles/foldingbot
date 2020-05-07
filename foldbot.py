217e99204cf1dda7bbdcae98a65b5dbcf22ad98d
Tangles <andyrthomson@gmail.com>
2020-05-08 01:11:02 +1000

    major refactor part 1 - move all score tracking to bot_data and remove individual chat-based score updates.
    This completely removes the need to add chat-based jobs to the job queue so no need to call /start anymore
    tracks what teams are 'subscribed to' in chats, and maintains a single set of scores for each of these even if
    a team is tracked in multiple chats.
    Score updates and milestome reporting are now chained directly to the job that updates the data files so this
    is now more timely and more efficient (since there's no need to periodically poll the last update timestamp).

--- foldbot.py
+++ foldbot.py
@@ -13,14 +13,17 @@ def init(context):
     log.info('init called - starting up.')
     if not context.bot_data.get('global_init', False):
         log.info('init - initialising global state from scratch...')
-        context.bot_data['global_init'] = True
+        context.bot_data['global_init'] = True # so we know we've been here (or read persistent state from file)
         context.bot_data['teamurl'] = 'https://apps.foldingathome.org/daily_team_summary.txt.bz2'
         context.bot_data['donorurl'] = 'https://apps.foldingathome.org/daily_user_summary.txt.bz2'
-        context.bot_data['lastmodt'] = datetime.fromtimestamp(0)
-        context.bot_data['lastmodd'] = datetime.fromtimestamp(0)
-        context.bot_data['teams'] = {}
-        context.bot_data['members'] = {}
-        context.bot_data['donors'] = {}
+        context.bot_data['lastmodt'] = datetime.fromtimestamp(0) # last modified header TEAMS url
+        context.bot_data['lastmodd'] = datetime.fromtimestamp(0) # last modified header DONORS url
+        context.bot_data['teams'] = {} # team data read from downloaed file
+        context.bot_data['members'] = {} # team scores broken down by member
+        context.bot_data['subs'] = {} # Chats subscribed to each team
+        context.bot_data['donors'] = {} # full list of donors with total wu/points and list of teams they contributed to
+        context.bot_data['milestones'] = [] # list of chat_ids that are subscribed to 'milestone' updates
+        context.bot_data['scores'] = {} # dict of curent scores per member per subscribed team (and team totals)
 
 def start(update, context):
     chat = update.effective_chat.id
@@ -30,17 +33,6 @@ def start(update, context):
                text="Folders of {0}, let's roll!".format(teams[context.chat_data['hometeam']]['name']))
     else:
         context.bot.send_message(chat_id=chat, text="I'm a folding bot, like Optimus Prime.")
-    # No references to jobs can be stored in bot/chat_data because Pickle will choke on it
-    # So we have to just make sure a job is running for each chat and give it a predictable name
-    # so we can find it in the queue again
-    for j in context.job_queue.get_jobs_by_name('updatescores-' + str(chat)):
-        print ('removing', j)
-        j.schedule_removal()
-    context.chat_data['lastcheck'] = datetime.fromtimestamp(0,tz=timezone.utc)
-    context.job_queue.run_repeating(updatescores, interval=300,
-                                    context=[chat, context.chat_data],
-                                    name='updatescores-' + str(chat))
-        
 
 def get_url():
     contents = requests.get('https://random.dog/woof.json').json()
@@ -53,22 +45,35 @@ def bop(update, context):
     context.bot.send_photo(chat_id=chat_id, photo=url)
 
 def setteam(update, context):
-    if len(context.args) > 0: 
-        context.chat_data['hometeam'] = context.args[0]
-        for j in context.job_queue.get_jobs_by_name('updatescores-' + str(update.effective_chat.id)):
-            print ('removing', j)
-            j.schedule_removal()
-        context.chat_data['lastcheck'] = datetime.fromtimestamp(0,tz=timezone.utc)
-        context.job_queue.run_repeating(updatescores, interval=300,
-                                    context=[update.effective_chat.id,
-                                    context.chat_data], name='updatescores-' + str(update.effective_chat.id))
-        
-        if context.args[0] not in context.bot_data['teams']:
-            context.bot.send_message(chat_id=update.effective_chat.id, text='WARNING: Team {0} not found.'.format(context.args[0]))
-        else: 
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text='Now following team: {0} - {1}.'.format(context.args[0],
-                                      context.bot_data['teams'][context.args[0]]['name']))
+    cd = context.chat_data
+    bd = context.bot_data
+    chat = update.effective_chat.id
+    log = logging.getLogger('foldbot')
+    if len(context.args) > 0:
+        # if 'hometeam' in cd and cd['hometeam'] in bd['subs'] and chat in bd['subs'][cd['hometeam']]:
+        # blah blah... forgiveness, not permission
+        try:
+           bd['subs'][cd['hometeam']].remove(chat)
+        except:
+           pass
+        try:
+           if len(bd['subs'][cd['hometeam']]) == 0:
+               del bd['subs'][cd['hometeam']]
+        except:
+           pass
+        tid = context.args[0]
+        cd['hometeam'] = tid
+        cd['lastcheck'] = datetime.fromtimestamp(0,tz=timezone.utc)
+        if tid not in bd['subs']:
+            bd['subs'][tid] = []
+        bd['subs'][tid].append(chat)
+
+        if context.args[0] not in bd['teams']:
+            context.bot.send_message(chat_id=chat, text='WARNING: Team {0} not found.'.format(tid))
+        else:
+            context.bot.send_message(chat_id=chat,
+                                     text='Now following team: {0} - {1}.'.format(tid,
+                                      bd['teams'][tid]['name']))
     else:
         tid = context.chat_data.get('hometeam', 'not set')
         context.bot.send_message(chat_id=update.effective_chat.id, text='Following team: {0}.'.format(tid))
@@ -77,12 +82,13 @@ def update_stats(context):
     # 1. if team data has refreshed, rebuild 'teams' dict with total score, rank, wu
     # 2. if donor data has refreshed, (a) rebuild 'donors' dict (sum of contributions per donor across teams)
     #                                 (b) rebuild 'members' dict (individual contributrions per donor to each team)
-    if 'teamurl' not in context.bot_data: return  # we have not run start yet, anywhere.
+    if 'teamurl' not in context.bot_data: return  # not initialised - should not happen
     teamurl = context.bot_data['teamurl']
     r = requests.head(teamurl)
     d = datetime.strptime(r.headers['last-modified'],'%a, %d %b %Y %H:%M:%S %Z')
     teams = context.bot_data['teams']
     log = logging.getLogger('foldbot')
+    updated = False
     if d > context.bot_data['lastmodt']:
         r = requests.get(teamurl)
         if r.ok:
@@ -110,13 +116,13 @@ def update_stats(context):
                         tbc = True
                         continue
                     else:
-                        # this is for the singular bag of dicks that put tabs in the team name 
+                        # this is for the singular bag of dicks that put tabs in the team name
                         # They lose everything after the first tab. Screw them.
                         (score, wu) = teamfields[-2:]
                 teams[team] = {k: v for k, v in (('name', name), ('score', score), ('wu', wu), ('rank', rank))}
             context.bot_data['teams'] = teams
             # timestamping to help chat-based update jobs know when there might be work to do
-            context.bot_data['updated'] = datetime.now(timezone.utc)
+            updated = True
     donorurl = context.bot_data['donorurl']
     r = requests.head(donorurl)
     d = datetime.strptime(r.headers['last-modified'],'%a, %d %b %Y %H:%M:%S %Z')
@@ -126,9 +132,9 @@ def update_stats(context):
             context.bot_data['lastmodd'] = d
             donors = {}
             members = {}
-            
+
             for donorline in bz2.decompress(r.content).splitlines()[2:]:
-                try: 
+                try:
                     (name, score, wu, team) = donorline.decode('utf-8').split('\t')
                 except:
                     log.warning('There was a problem with the following donor: ' + donorline.decode('utf-8'))
@@ -142,7 +148,7 @@ def update_stats(context):
                 members[team][name] = { 'score': score, 'wu': wu }
                 # assumes the donor list is sorted by max score, which it appears to be
                 members[team][name]['teamrank'] = len(members[team].keys())
-                 
+
             # Now the fun part: rank each donor by total score.
             rank = 0
             for donor in sorted(donors, key=lambda x: donors[x]['score'], reverse=True):
@@ -150,50 +156,56 @@ def update_stats(context):
                 donors[donor]['rank'] = rank
             context.bot_data['donors'] = donors
             context.bot_data['members'] = members
-            context.bot_data['updated'] = datetime.now(timezone.utc)
+            updated = True
+    if updated:
+        updatescores(context)
 
 def setmilestones(update, context):
     team = None
+    chat = update.effective_chat.id
     if 'hometeam' in context.chat_data:
         team = context.chat_data['hometeam'] #use team id if can't lookup name
         if team in context.bot_data['teams']:
             team = context.bot_data['teams'][team]['name']
     teamstr = " for team " + team if team else ""
     if len(context.args) == 0:
-        msr = "" if context.chat_data.get('milestones', False) else " not"
-        context.bot.send_message(chat_id=update.effective_chat.id, text='milestones are{0} set{1}.'.format(msr, teamstr))
+        msr = "" if chat in context.bot_data['milestones'] else " not"
+        context.bot.send_message(chat_id=chat, text='milestones are{0} set{1}.'.format(msr, teamstr))
         return
     if len(context.args) == 1:
         if context.args[0] == 'on':
-            if context.chat_data.get('milestones', False):
-                context.bot.send_message(chat_id=update.effective_chat.id,
+            if chat in context.bot_data['milestones']:
+                context.bot.send_message(chat_id=chat,
                                          text='milestones are already being reported{0}.'.format(teamstr))
                 return
-            context.chat_data['milestones'] = True
-                                                                        
+            context.bot_data['milestones'].append(chat)
+
             if not team:
-                context.bot.send_message(chat_id=update.effective_chat.id,
+                context.bot.send_message(chat_id=chat,
                                          text='WARNING: milestones set, but no team to report on\nUse /team to set a team')
             else:
-                context.bot.send_message(chat_id=update.effective_chat.id, text='milestones will be reported{0}.'.format(teamstr))
-              
+                context.bot.send_message(chat_id=chat, text='milestones will be reported{0}.'.format(teamstr))
             return
+
         if context.args[0] == 'off':
-            if not context.chat_data.get('milestones', False):
-                context.bot.send_message(chat_id=update.effective_chat.id,
+            if chat not in context.bot_data['milestones']:
+                context.bot.send_message(chat_id=chat,
                                          text='milestone reporting is already off{0}.'.format(teamstr))
                 return
-            context.chat_data['milestones'] = False
-            context.bot.send_message(chat_id=update.effective_chat.id, text='milestones will not be reported{0}.'.format(teamstr))
+            context.bot_data['milestones'].remove(chat)
+            context.bot.send_message(chat_id=chat, text='milestones will not be reported{0}.'.format(teamstr))
             return
     context.bot.send_message(chat_id=update.effective_chat.id, text='usage: /milestones [on|off]')
 
+def send_milestone(context, team, message):
+    for chat in context.bot_data['subs'][team]:
+        if chat in context.bot_data['milestones']:
+            context.bot.send_message(chat_id=chat,  disable_notification=True, text=message)
+
 def updatescores(context):
-    chat_id = context.job.context[0]
-    chat_data = context.job.context[1]
-    if 'hometeam' not in chat_data: return # nothing to do
-    if chat_data['lastcheck'] > context.bot_data['updated']: return
-    chat_data['lastcheck'] = datetime.now(timezone.utc)
+    #chat_id = context.job.context[0]
+    #chat_data = context.job.context[1]
+    #if 'hometeam' not in chat_data: return # nothing to do
     # Pretty formatting
     numbers = { 1000000: "one million",
                 2000000: "two million",
@@ -209,70 +221,70 @@ def updatescores(context):
              5000000000: "five billion" }
 
     # some shorthand references
-    team = chat_data['hometeam']
     teams = context.bot_data['teams']
     donors = context.bot_data['donors']
     members = context.bot_data['members']
-    if team not in teams: return # probably should warn here
-    teamname = teams[team]['name']
-    newscores = {}
-    newscores[teamname] = { 'teamrank': 0, 'fullrank': teams[team]['rank'],
+    subs = context.bot_data['subs']
+    for team in subs:
+        if team not in teams: return # probably should warn here
+        teamname = teams[team]['name']
+        newscores = {}
+        newscores[teamname] = { 'teamrank': 0, 'fullrank': teams[team]['rank'],
                                       'wu': teams[team]['wu'], 'score': teams[team]['score'] }
-    for name in members[team]:
-        newscores[name] = { 'teamrank': members[team][name]['teamrank'], 'fullrank': donors[name]['rank'],
+        for name in members[team]:
+            newscores[name] = { 'teamrank': members[team][name]['teamrank'], 'fullrank': donors[name]['rank'],
                                       'wu': members[team][name]['wu'], 'score': members[team][name]['score'] }
-    if 'scores' in chat_data and teamname in chat_data['scores'] and chat_data.get('milestones',False):
-        # not first call, didn't switch teams, and reporting is on
-        scores = chat_data['scores']
-        for name in newscores.keys():
-            forteam = " for " + teamname if name != teamname else ""
-            if name not in scores:
-                context.bot.send_message(chat_id=chat_id, disable_notification=True,
-                                         text='New member: {0} has joined {1}!'.format(name, teamname))
-            elif name != teamname and newscores[name]['teamrank'] < scores[name]['teamrank']:
-                context.bot.send_message(chat_id=chat_id,
-                                         text='{0} has advanced to rank {1} in {2}!'.format(name,
+        if team in context.bot_data['scores']:
+            scores = context.bot_data['scores'][team]
+            for name in newscores.keys():
+                forteam = " for " + teamname if name != teamname else ""
+                if name not in scores:
+                    send_milestone(context, team,
+                                   'New member: {0} has joined {1}!'.format(name, teamname))
+                elif name != teamname and newscores[name]['teamrank'] < scores[name]['teamrank']:
+                    send_milestone(context, team,
+                                   '{0} has advanced to rank {1} in {2}!'.format(name,
                                               newscores[name]['teamrank'], teamname))
-            done = False
-            steps = [1, 2, 5]
-            while not done and name in scores: # too lazy to come up with a smart way to do this
-                for step in steps:
-                    done = True
-                    if step >= newscores[name]['fullrank']:
-                        if step < scores[name]['fullrank']:
-                           context.bot.send_message(chat_id=chat_id, disable_notification=True,
-                               text='{0} is now in the overall top {1}!'.format(name, numbers.get(step,step)))
+                done = False
+                steps = [1, 2, 5]
+                while not done and name in scores: # too lazy to come up with a smart way to do this
+                    for step in steps:
+                        done = True
+                        if step >= newscores[name]['fullrank']:
+                            if step < scores[name]['fullrank']:
+                                send_milestone(context, team,
+                                '{0} is now in the overall top {1}!'.format(name, numbers.get(step,step)))
                     else:
                         done = False
                     if step > int(scores[name]['wu']):
                         if step <= int(newscores[name]['wu']):
-                           context.bot.send_message(chat_id=chat_id, disable_notification=True,
-                               text='{0} has processed {1} work units{2}!'.format(name, numbers.get(step,step), forteam))
-                    else:
-                        done = False
-                    if step > int(scores[name]['score']):
-                        if step <= int(newscores[name]['score']):
-                           context.bot.send_message(chat_id=chat_id, disable_notification=True,
-                               text='{0} has earned {1} credit{2}!'.format(name, numbers.get(step,step), forteam))
-                    else:
-                        done = False
-                # https://stackoverflow.com/questions/4081217/how-to-modify-list-entries-during-for-loop
-                steps[:] = [x * 10 for x in steps]
+                           send_milestone(context, team,
+                               '{0} has processed {1} work units{2}!'.format(name, numbers.get(step,step), forteam))
+                        else:
+                            done = False
+                        if step > int(scores[name]['score']):
+                            if step <= int(newscores[name]['score']):
+                               send_milestone(context, team,
+                               '{0} has earned {1} credit{2}!'.format(name, numbers.get(step,step), forteam))
+                        else:
+                            done = False
+                    # https://stackoverflow.com/questions/4081217/how-to-modify-list-entries-during-for-loop
+                    steps[:] = [x * 10 for x in steps]
 
-    # done reporting, update values.
-    chat_data['scores'] = newscores
+        # done reporting, update values.
+        context.bot_data['scores'][team] = newscores
 
 def getstats(update, context):
     if 'hometeam' not in context.chat_data:
         context.bot.send_message(chat_id=update.effective_chat.id, text='Please set a team with /team first.')
         return
-    if 'scores' not in context.chat_data:
+    team = context.chat_data['hometeam']
+    if  team not in context.bot_data['scores']:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry I've just woken up.  No stats yet")
         return
-    team = context.chat_data['hometeam']
     teams = context.bot_data['teams']
     teamname = teams[team]['name']
-    scores = context.chat_data['scores']
+    scores = context.bot_data['scores'][team]
     message = 'Team: {0}({name})\nCredit: {score} Rank: {rank} WU: {wu}.'.format(team, **teams[team])
     for name in sorted(scores, key=lambda x: scores[x]['teamrank']):
         if name == teamname: continue
@@ -280,7 +292,6 @@ def getstats(update, context):
         message += '\n{teamrank: >2}.{frank: >8} {name: <16}Cr:{score: >9} WU:{wu: >4}'.format(name=name, frank=frank, **scores[name])
     #print("Stats message: " + message)
     context.bot.send_message(chat_id=update.effective_chat.id, text='`' + message + '`', parse_mode=ParseMode.MARKDOWN_V2)
-        
 
 def main():
     updater = Updater(token=BOTTOKEN,
@@ -296,8 +307,7 @@ def main():
     dp.add_handler(CommandHandler('team',setteam))
     dp.add_handler(CommandHandler('milestones',setmilestones))
     dp.add_handler(CommandHandler('stats',getstats))
-    #dp.add_handler(CommandHandler('stats',setstats))
-    jq.run_repeating(update_stats, interval=900, first=30)
+    jq.run_repeating(update_stats, interval=600, first=10)
     updater.start_polling()
     updater.idle()
 
